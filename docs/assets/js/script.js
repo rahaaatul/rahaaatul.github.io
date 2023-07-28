0d456591eb8c2680d84647c7ba17555923b6bc6f
Rahatul Ghazi <86149861+rahaaatul@users.noreply.github.com>
2023-07-29 00:34:46 +0600

    I did something
    but I don't know what.

--- docs/assets/js/script.js
+++ docs/assets/js/script.js
@@ -1,34 +1,42 @@
 // script.js
-// Get the elements for profile card and repositories container
-const profileCard = document.getElementById("profile");
-const repositoriesContainer = document.getElementById("repositories");
+// Function to toggle the visibility of the profile and repositories sections
+function toggleSections() {
+  var profileContainer = document.getElementById("profile");
+  var repositoriesContainer = document.getElementById("repositories");
 
-// Function to show the profile card and hide repositories container
-function showProfile() {
-    repositoriesContainer.classList.add("slideLeftFadeOut");
-    profileCard.classList.add("slideRightFadeIn");
+  // Determine which container is currently hidden and which one is visible
+  var isProfileHidden = profileContainer.classList.contains("hidden");
+  var isRepositoriesHidden = repositoriesContainer.classList.contains("hidden");
 
-    repositoriesContainer.addEventListener("animationend", () => {
-        repositoriesContainer.style.display = "none";
-        repositoriesContainer.classList.remove("slideLeftFadeOut");
-    });
-    profileCard.addEventListener("animationend", () => {
-        profileCard.style.display = "block";
-        profileCard.classList.remove("slideRightFadeIn");
-    });
+  // If both containers are hidden, show the profile container
+  if (isProfileHidden && isRepositoriesHidden) {
+    profileContainer.classList.remove("hidden");
+    profileContainer.classList.add("slide-in-left");
+  }
+  // If profile container is visible, hide it and show the repositories container
+  else if (!isProfileHidden && isRepositoriesHidden) {
+    profileContainer.classList.add("slide-out-center");
+    repositoriesContainer.classList.remove("hidden");
+    repositoriesContainer.classList.add("slide-in-right");
+  }
+  // If repositories container is visible, hide it and show the profile container
+  else {
+    profileContainer.classList.remove("hidden");
+    profileContainer.classList.add("slide-in-left");
+    repositoriesContainer.classList.add("slide-out-right");
+  }
+
+  // Toggle the "hidden" class for both containers after a delay to allow animations to start
+  setTimeout(function () {
+    profileContainer.classList.toggle("hidden");
+    repositoriesContainer.classList.toggle("hidden");
+  }, 100); // Adjust the delay as needed
 }
 
-// Function to show the repositories container and hide profile card
-function showRepositories() {
-    profileCard.classList.add("slideRightFadeOut");
-    repositoriesContainer.classList.add("slideLeftFadeIn");
-
-    profileCard.addEventListener("animationend", () => {
-        profileCard.style.display = "none";
-        profileCard.classList.remove("slideRightFadeOut");
-    });
-    repositoriesContainer.addEventListener("animationend", () => {
-        repositoriesContainer.style.display = "block";
-        repositoriesContainer.classList.remove("slideLeftFadeIn");
-    });
-}
+// Add event listener to the parent element of the button for delegated event handling
+document.querySelector(".social-links").addEventListener("click", function (event) {
+  // Check if the click target is the button with the class "icon-button"
+  if (event.target.classList.contains("icon-button")) {
+    toggleSections();
+  }
+});
