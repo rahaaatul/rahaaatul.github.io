1649e5a350364761ccbea4fc337fea2eefea3fee
Rahatul Ghazi <86149861+rahaaatul@users.noreply.github.com>
2023-07-29 01:52:20 +0600

    no idea

--- docs/assets/js/script.js
+++ docs/assets/js/script.js
@@ -1,5 +1,40 @@
 // script.js
 // Function to toggle the visibility of the profile and repositories sections
+//
+// This function determines which container is currently hidden and which one is visible,
+// and then toggles the visibility of the two containers accordingly.
+// The function also uses a delay to allow the animations to start before the "hidden" class is toggled.
+function toggleSections() {
+  var profileContainer = document.getElementById("profile");
+  var repositoriesContainer = document.getElementById("repositories");
+
+  // Determine which container is currently hidden and which one is visible
+  var isProfileHidden = profileContainer.classList.contains("hidden");
+  var isRepositoriesHidden = repositoriesContainer.classList.contains("hidden");
+
+  // If both containers are hidden, show the profile container
+  if (isProfileHidden && isRepositoriesHidden) {
+    profileContainer.classList.remove("hidden");
+    profileContainer.classList.add("slide-in-left");
+  }
+  // If profile container is visible, hide it and show the repositories container
+  else if (!isProfileHidden && isRepositoriesHidden) {
+    profileContainer.classList.add("slide-out-center");
+    repositoriesContainer.classList.remove("hidden");
+    repositoriesContainer.classList.add("slide-in-right");
+    repositoriesContainer.addEventListener("animationend", function () {
+      repositoriesContainer.classList.toggle("hidden");
+    });
+  }
+  // If repositories container is visible, hide it and show the profile container
+  else {
+    profileContainer.classList.remove("hidden");
+    profileContainer.classList.add("slide-in-left");
+    repositoriesContainer.classList.add("slide-out-center");
+    repositoriesContainer.addEventListener("animationend", function () {
+      repositoriesContainer.classList.toggle("hidden");
+    });
+  }// Function to toggle the visibility of the profile and repositories sections
 function toggleSections() {
   var profileContainer = document.getElementById("profile");
   var repositoriesContainer = document.getElementById("repositories");
@@ -23,7 +58,7 @@ function toggleSections() {
   else {
     profileContainer.classList.remove("hidden");
     profileContainer.classList.add("slide-in-left");
-    repositoriesContainer.classList.add("slide-out-right");
+    repositoriesContainer.classList.add("slide-out-center");
   }
 
   // Toggle the "hidden" class for both containers after a delay to allow animations to start
@@ -33,10 +68,32 @@ function toggleSections() {
   }, 100); // Adjust the delay as needed
 }
 
-// Add event listener to the parent element of the button for delegated event handling
-document.querySelector(".social-links").addEventListener("click", function (event) {
-  // Check if the click target is the button with the class "icon-button"
-  if (event.target.classList.contains("icon-button")) {
-    toggleSections();
-  }
-});
+// Add event listener to the button for toggling sections
+document.querySelector(".icon-button").addEventListener("click", toggleSections);
+
+// Add event listener to the "Back to Profile" link inside the repositories container
+document
+  .querySelector(".repository-list a[href='#profile']")
+  .addEventListener("click", function (event) {
+    event.preventDefault(); // Prevent the default link behavior
+    toggleSections(); // Toggle the sections visibility
+  });
+
+
+  // Toggle the "hidden" class for both containers after a delay to allow animations to start
+  setTimeout(function () {
+    profileContainer.classList.toggle("hidden");
+    repositoriesContainer.classList.toggle("hidden");
+  }, 100); // Adjust the delay as needed
+}
+
+// Add event listener to the button for toggling sections
+document.querySelector(".icon-button").addEventListener("click", toggleSections);
+
+// Add event listener to the "Back to Profile" link inside the repositories container
+document
+  .querySelector(".repository-list a[href='#profile']")
+  .addEventListener("click", function (event) {
+    event.preventDefault(); // Prevent the default link behavior
+    toggleSections(); // Toggle the sections visibility
+  });
