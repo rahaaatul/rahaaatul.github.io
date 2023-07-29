a81df3b1fadabbdb334f27868f7f3ba84247edb0
Rahatul Ghazi <86149861+rahaaatul@users.noreply.github.com>
2023-07-29 14:25:46 +0600

    something something

--- docs/assets/js/script.js
+++ docs/assets/js/script.js
@@ -1,99 +1,78 @@
-// script.js
+// Variable to control whether animations should be applied
+var animatePage = true;
+
 // Function to toggle the visibility of the profile and repositories sections
-//
-// This function determines which container is currently hidden and which one is visible,
-// and then toggles the visibility of the two containers accordingly.
-// The function also uses a delay to allow the animations to start before the "hidden" class is toggled.
 function toggleSections() {
   var profileContainer = document.getElementById("profile");
   var repositoriesContainer = document.getElementById("repositories");
 
-  // Determine which container is currently hidden and which one is visible
+  // Check the initial state of the containers
   var isProfileHidden = profileContainer.classList.contains("hidden");
   var isRepositoriesHidden = repositoriesContainer.classList.contains("hidden");
 
-  // If both containers are hidden, show the profile container
-  if (isProfileHidden && isRepositoriesHidden) {
-    profileContainer.classList.remove("hidden");
-    profileContainer.classList.add("slide-in-left");
-  }
-  // If profile container is visible, hide it and show the repositories container
-  else if (!isProfileHidden && isRepositoriesHidden) {
-    profileContainer.classList.add("slide-out-center");
-    repositoriesContainer.classList.remove("hidden");
-    repositoriesContainer.classList.add("slide-in-right");
-    repositoriesContainer.addEventListener("animationend", function () {
-      repositoriesContainer.classList.toggle("hidden");
-    });
-  }
-  // If repositories container is visible, hide it and show the profile container
-  else {
-    profileContainer.classList.remove("hidden");
-    profileContainer.classList.add("slide-in-left");
-    repositoriesContainer.classList.add("slide-out-center");
-    repositoriesContainer.addEventListener("animationend", function () {
-      repositoriesContainer.classList.toggle("hidden");
-    });
-  }// Function to toggle the visibility of the profile and repositories sections
-function toggleSections() {
-  var profileContainer = document.getElementById("profile");
-  var repositoriesContainer = document.getElementById("repositories");
+  // Show both containers initially
+  profileContainer.style.display = "block";
+  repositoriesContainer.style.display = "block";
 
-  // Determine which container is currently hidden and which one is visible
-  var isProfileHidden = profileContainer.classList.contains("hidden");
-  var isRepositoriesHidden = repositoriesContainer.classList.contains("hidden");
+  // Toggle the "hidden" class for both containers
+  profileContainer.classList.toggle("hidden");
+  repositoriesContainer.classList.toggle("hidden");
 
-  // If both containers are hidden, show the profile container
-  if (isProfileHidden && isRepositoriesHidden) {
-    profileContainer.classList.remove("hidden");
-    profileContainer.classList.add("slide-in-left");
-  }
-  // If profile container is visible, hide it and show the repositories container
-  else if (!isProfileHidden && isRepositoriesHidden) {
-    profileContainer.classList.add("slide-out-center");
-    repositoriesContainer.classList.remove("hidden");
-    repositoriesContainer.classList.add("slide-in-right");
-  }
-  // If repositories container is visible, hide it and show the profile container
-  else {
-    profileContainer.classList.remove("hidden");
-    profileContainer.classList.add("slide-in-left");
-    repositoriesContainer.classList.add("slide-out-center");
+  // Apply the animations only if animatePage flag is true
+  if (animatePage) {
+    if (!isProfileHidden && isRepositoriesHidden) {
+      // Profile is visible, repositories are hidden
+      profileContainer.classList.remove("slide-out-center");
+      profileContainer.classList.add("slide-in-left");
+    } else if (isProfileHidden && !isRepositoriesHidden) {
+      // Repositories are visible, profile is hidden
+      repositoriesContainer.classList.remove("slide-out-center-repo");
+      repositoriesContainer.classList.add("slide-in-right-repo");
+    } else if (isProfileHidden && isRepositoriesHidden) {
+      // Both are hidden, show profile (you can adjust which one to show first)
+      profileContainer.style.display = "block";
+      profileContainer.classList.remove("slide-out-center");
+      profileContainer.classList.add("slide-in-left");
+    } else {
+      // Both are visible, hide repositories
+      repositoriesContainer.classList.remove("slide-in-right");
+      repositoriesContainer.classList.add("slide-out-center-repo");
+    }
   }
 
-  // Toggle the "hidden" class for both containers after a delay to allow animations to start
-  setTimeout(function () {
-    profileContainer.classList.toggle("hidden");
-    repositoriesContainer.classList.toggle("hidden");
-  }, 100); // Adjust the delay as needed
+  // Hide the profile and repositories containers
+  if (profileContainer.classList.contains("hidden")) {
+    profileContainer.style.display = "none";
+  }
+  if (repositoriesContainer.classList.contains("hidden")) {
+    repositoriesContainer.style.display = "none";
+  }
 }
 
-// Add event listener to the button for toggling sections
-document.querySelector(".icon-button").addEventListener("click", toggleSections);
+// Rest of the script remains unchanged
+
+// Function to handle the "Repositories" button click event
+function onRepositoriesButtonClick(event) {
+  event.preventDefault(); // Prevent the default link behavior
+  toggleSections(); // Toggle the sections visibility
+}
+
+// Function to handle the "Back to Profile" link click event
+function onBackToProfileLinkClick(event) {
+  event.preventDefault(); // Prevent the default link behavior
+  toggleSections(); // Toggle the sections visibility
+}
+
+// Add event listener to the "Repositories" button
+document.querySelector(".icon-button").addEventListener("click", onRepositoriesButtonClick);
 
 // Add event listener to the "Back to Profile" link inside the repositories container
 document
   .querySelector(".repository-list a[href='#profile']")
-  .addEventListener("click", function (event) {
-    event.preventDefault(); // Prevent the default link behavior
-    toggleSections(); // Toggle the sections visibility
-  });
+  .addEventListener("click", onBackToProfileLinkClick);
 
+// NOTE: The lines below have been commented out to make both sections visible from the start.
+// If you want to hide one of the sections initially, uncomment the appropriate line.
 
-  // Toggle the "hidden" class for both containers after a delay to allow animations to start
-  setTimeout(function () {
-    profileContainer.classList.toggle("hidden");
-    repositoriesContainer.classList.toggle("hidden");
-  }, 100); // Adjust the delay as needed
-}
-
-// Add event listener to the button for toggling sections
-document.querySelector(".icon-button").addEventListener("click", toggleSections);
-
-// Add event listener to the "Back to Profile" link inside the repositories container
-document
-  .querySelector(".repository-list a[href='#profile']")
-  .addEventListener("click", function (event) {
-    event.preventDefault(); // Prevent the default link behavior
-    toggleSections(); // Toggle the sections visibility
-  });
+// document.getElementById("profile").classList.add("hidden"); // Hide the profile initially
+document.getElementById("repositories").classList.add("hidden"); // Hide the repositories initially
